25b14a400c0f2c0699d3c8fe12356cca127b23ce
pedrodalvy <pedrodalvy@outlook.com>
2019-12-23 19:53:37 -0400

    criacao da tabela associativa entre imoveis e proprietarios

--- app/ImovelProprietario.php
+++ app/ImovelProprietario.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class ImovelProprietario extends Model
 {
-    //
+    protected $table = 'imovel_proprietario';
 }
--- database/migrations/2019_12_23_234533_create_imoveis_proprietarios_table.php
+++ database/migrations/2019_12_23_234533_create_imoveis_proprietarios_table.php
@@ -15,7 +15,17 @@ class CreateImoveisProprietariosTable extends Migration
     {
         Schema::create('imoveis_proprietarios', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('imovel_id');
+            $table->foreign('imovel_id')->references('id')->on('imoveis');
+
+            $table->unsignedBigInteger('proprietario_id');
+            $table->foreign('proprietario_id')->references('id')->on('proprietarios');
+
+            $table->boolean('responsavel');
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
