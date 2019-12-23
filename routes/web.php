41e2c2523ac665dbdcb00a77a123f66b34cd73da
pedrodalvy <pedrodalvy@outlook.com>
2019-12-23 17:54:54 -0400

    Criação das rotas basicas do sistema

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,42 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => '/inquilino'], function () {
+    Route::get('/', 'InquilinosController@lista')->name('inquilinoListar');
+
+    Route::get('cadastro', 'InquilinosController@formularioCadastrar')->name('inquilinoFormularioCadastrar');
+    Route::post('cadastro', 'InquilinosController@cadastrar')->name('inquilinoCadastrar');
+
+    Route::get('{id}/editar', 'InquilinosController@formularioEditar')->name('inquilinoFormularioEditar');
+    Route::post('{id}/editar', 'InquilinosController@editar')->name('inquilinoEditar');
+
+    Route::post('{id}/remover', 'InquilinosController@remover')->name('inquilinoRemover');
+});
+
+
+Route::group(['prefix' => '/imoveis'], function () {
+    Route::get('/', 'ImoveisController@lista')->name('imovelListar');
+
+    Route::get('cadastro', 'ImoveisController@formularioCadastrar')->name('imovelFormularioCadastrar');
+    Route::post('cadastro', 'ImoveisController@cadastrar')->name('imovelCadastrar');
+
+    Route::get('{id}/editar', 'ImoveisController@formularioEditar')->name('imovelFormularioEditar');
+    Route::post('{id}/editar', 'ImoveisController@editar')->name('imovelEditar');
+
+    Route::post('{id}/remover', 'ImoveisController@remover')->name('imovelRemover');
+});
+
+
+Route::group(['prefix' => '/proprietarios'], function () {
+    Route::get('/', 'ProprietariosController@lista')->name('proprietariosListar');
+
+    Route::get('cadastro', 'ProprietariosController@formularioCadastrar')->name('proprietariosFormularioCadastrar');
+    Route::post('cadastro', 'ProprietariosController@cadastrar')->name('proprietariosCadastrar');
+
+    Route::get('{id}/editar', 'ProprietariosController@formularioEditar')->name('proprietariosFormularioEditar');
+    Route::post('{id}/editar', 'ProprietariosController@editar')->name('proprietariosEditar');
+
+    Route::post('{id}/remover', 'ProprietariosController@remover')->name('proprietariosRemover');
+});
