b96de1111abd22b44751b60c4dcec5c3c38fdcdd
pedrodalvy <pedrodalvy@outlook.com>
2019-12-24 09:49:24 -0400

    Criado metodo para cadastro de imoveis e endereços

---
+++ app/Http/Controllers/EnderecoController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Endereco;
+use Illuminate\Http\Request;
+
+class EnderecoController extends Controller
+{
+    static function cadastrar(array $attributes = [])
+    {
+        if (!$attributes) return false;
+
+        $endereco = new Endereco();
+
+        $endereco->cep = $attributes['cep'];
+        $endereco->logradouro = $attributes['logradouro'];
+        $endereco->numero = $attributes['numero'];
+        $endereco->complemento = $attributes['complemento'];
+        $endereco->bairro = $attributes['bairro'];
+        $endereco->municipio_id = $attributes['municipio_id'];
+        $endereco->save();
+
+        if ($endereco->id) {
+            return $endereco->id;
+        }
+
+        return false;
+    }
+}
--- app/Http/Controllers/ImoveisController.php
+++ app/Http/Controllers/ImoveisController.php
@@ -2,24 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Endereco;
+use App\Imovel;
 use Illuminate\Http\Request;
 
 class ImoveisController extends Controller
 {
     public function lista()
     {
-        return 'Implementar lista com todos os imoveis';
+        $imoveis = Imovel::all();
+
+        return view('imoveis.lista', compact('imoveis'));
     }
 
 
     public function formularioCadastrar()
     {
-        return 'Implementar formulario para cadastro de imoveis';
+        return view('imoveis.formularioCadastrar');
     }
 
-    public function cadastrar()
+    public function cadastrar(Request $request)
     {
-        return 'Implementar cadastro de imovel no banco de dados';
+        $imovelEndereco = null;
+
+        if ($request->cep) {
+            $endereco = [
+                'cep' => $request->get('cep'),
+                'logradouro' => $request->get('logradouro'),
+                'numero' => $request->get('numero'),
+                'complemento' => $request->get('complemento'),
+                'bairro' => $request->get('bairro'),
+                'municipio_id' => $request->get('municipio_id')
+            ];
+
+            $imovelEndereco = EnderecoController::cadastrar($endereco);
+        }
+
+        if (!$imovelEndereco) return 'Falha ao cadastrar endereço';
+
+        $imovel = new Imovel();
+
+        $imovel->descricao = $request->get('descricao');
+        $imovel->inscricao_imobiliaria = $request->get('inscricao_imobiliaria');
+        $imovel->valor_venda = $request->get('valor_venda');
+        $imovel->valor_aluguel = $request->get('valor_aluguel');
+        $imovel->endereco_id = $imovelEndereco;
+        $imovel->save();
+
+        if ($imovel->id) {
+            return redirect()->to(route('imovelListar'));
+        }
+
+        return 'Falha ao cadastrar o imóvel';
     }
 
 
