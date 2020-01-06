438fda00c1d9eb038371c367832d5f4a50aa6ff0
pedrodalvy <pedrodalvy@outlook.com>
2020-01-06 17:28:34 -0400

    Configurando sidenav do projeto

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,20 +2,27 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
+    
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+
     <title>@yield('title','Gerenciador de Aluguéis')</title>
-    <style>
-        html, body {
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-        }
-    </style>
+
 </head>
-<body style="width: 99%">
+<body>
 
 <div class="row">
-    <div class="container">
+
+    <div class="sidenav col-md-2">
+        <a href="{{ route('imovel.index') }}">Imóveis</a>
+        <a href="{{ route('proprietarios.index') }}">Proprietários</a>
+        <a href="{{ route('inquilinos.index') }}">Inquilinos</a>
+    </div>
+
+    <div class="container col-md-10" >
         @yield('content')
     </div>
 </div>
