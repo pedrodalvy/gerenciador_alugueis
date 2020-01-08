bd33834dc1a5f8befabd2cec6f8d11769d3cedc7
pedrodalvy <pedrodalvy@outlook.com>
2020-01-08 19:14:46 -0400

    Correção na mensagem de validação do formulario de cadastro de imóveis

--- resources/views/admin/imoveis/_form.blade.php
+++ resources/views/admin/imoveis/_form.blade.php
@@ -20,34 +20,35 @@
 
 <br>
 <p>Endereço</p>
+
 {{ Form::hidden('endereco_id', $imovel['endereco_id']) }}
 
-@component('form._form_group', ['field' => 'descricao'])
+@component('form._form_group', ['field' => 'cep'])
     {{ Form::label('cep', 'CEP', ['class' => 'control-label']) }}
     {{ Form::text('cep', null, ['class' => 'form-control']) }}
 @endcomponent
 
-@component('form._form_group', ['field' => 'descricao'])
+@component('form._form_group', ['field' => 'logradouro'])
     {{ Form::label('logradouro', 'Logradouro', ['class' => 'control-label']) }}
     {{ Form::text('logradouro', null, ['class' => 'form-control']) }}
 @endcomponent
 
-@component('form._form_group', ['field' => 'descricao'])
+@component('form._form_group', ['field' => 'numero'])
     {{ Form::label('numero', 'Número', ['class' => 'control-label']) }}
     {{ Form::text('numero', null, ['class' => 'form-control']) }}
 @endcomponent
 
-@component('form._form_group', ['field' => 'descricao'])
+@component('form._form_group', ['field' => 'complemento'])
     {{ Form::label('complemento', 'Complemento', ['class' => 'control-label']) }}
     {{ Form::text('complemento', null, ['class' => 'form-control']) }}
 @endcomponent
 
-@component('form._form_group', ['field' => 'descricao'])
+@component('form._form_group', ['field' => 'bairro'])
     {{ Form::label('bairro', 'Bairro', ['class' => 'control-label']) }}
     {{ Form::text('bairro', null, ['class' => 'form-control']) }}
 @endcomponent
 
-@component('form._form_group', ['field' => 'descricao'])
+@component('form._form_group', ['field' => 'municipio_id'])
     {{ Form::label('municipio_id', 'Município', ['class' => 'control-label']) }}
     {{ Form::text('municipio_id', null, ['class' => 'form-control']) }}
 @endcomponent
